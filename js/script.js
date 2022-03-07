cb8098a3752c537157f3b85877864ebbbd3e2469
Jorge Carrillo <jorgecardi00@gmail.com>
2022-03-07 06:52:52 -0500

    Commit 7: Se arreglan errores de conexión del URL Json y se have el primer intento de uso de libreria de graficación

--- js/script.js
+++ js/script.js
@@ -1,13 +1,64 @@
 const Json =  () => {
     const url = document.getElementById("url").value;
-    fetch(`${url}`,{
-        method: "get",
-    }).then(  response => response.json())
-    .then(data => console.log(data));
+    fetch(`${url}`)
+        .then(response => response.json())
+        .then(data => printCharts(data));
+   };
+
+//function Json() {
+    //fetch("https://www.datos.gov.co/resource/vv8g-8u9u.json")
+    //.then(response => response.json())
+    //.then(data => printCharts(data));
+//};
+
+function printCharts(repositorio){
+    document.body.classList.add('running') //para que aparezca en el front los canvas y cambiar la propiedad en el css
+   // console.log(repositorio);
+   compareRadialChart(repositorio,'chart2')
+}
+
+
+
+function compareRadialChart(repositorio, id){  //función para grafico radial
+    const data = {  //parametros de data
+        labels: ['Copacabana', 'Caldas', 'Itagui','Medellin', 'Rionegro'],
+        datasets:[{
+            data: [
+                repositorio.filter(eachData => eachData.nombre_municipio_curso === 'LA ESTRELLA').length,
+                repositorio.filter(eachData => eachData.nombre_municipio_curso === 'CALDAS').length,
+                repositorio.filter(eachData => eachData.nombre_municipio_curso === 'ITAGUI').length,
+                repositorio.filter(eachData => eachData.nombre_municipio_curso === 'MEDELLIN').length,
+                repositorio.filter(eachData => eachData.nombre_municipio_curso === 'BELLO').length
+            ],
+            borderWidth:1,
+            borderColor:styles.color.solids.map(eachColor => eachColor),
+            backgroundColor:styles.color.alphas.map(eachColor => eachColor),
+        }]
     }
 
-//const abrirJson = () => {
-    //fetch("https://www.datos.gov.co/resource/gpje-sixt.json")
-    //.then(response => response.json())
-    //.then(data => console.log(data));
-//};
+    const options = {   //estilos y opciones del grafico
+        scale:{
+            gridLines:{ //lineas 
+                color: '#444'
+            },
+            ticks:{ //quitar etiquetas 
+                display: false
+            }
+        },
+        legend:{
+            position:'right',
+            labels:{
+                fontColor:'#fff'
+            }
+        }
+    }
+
+    new Chart('chart2',{type: 'polarArea',data, options})
+}
+
+//let url = document.getElementById("url").value;
+
+//fetch(url)
+///.then(res => res.json())
+//})
+//https://www.datos.gov.co/resource/gt2j-8ykr.json
