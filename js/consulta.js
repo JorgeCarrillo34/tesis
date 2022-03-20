b4b9b0fe217ffbf90018a855647e15d3825782ba
Cristian Duarte <duartec688@gmail.com>
2022-03-20 17:06:37 -0500

    Commit 13: Se ajusta la interfaz de bases de datos y se crea la conexión

--- js/consulta.js
+++ js/consulta.js
@@ -4,10 +4,11 @@ const mysqlConexion = require('./database');
 
 mysqlConexion.query('SHOW TABLES from repos', (err,rows)=>{
     if(!err){
-        
+
         res.json(rows[0]);
     }
     else{
         console.log(err);
+        
     }
 })
