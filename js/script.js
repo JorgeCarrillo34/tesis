46fd1188aa4db69c8379e06ad474f63a5d2d7c8c
Jorge Carrillo <jorgecardi00@gmail.com>
2022-04-02 22:20:17 -0500

    Commit 20: Se agregan funciones para obtencion de campos y valores por repositorio, además se grefican con campos seleccionados

--- js/script.js
+++ js/script.js
@@ -1,152 +1,363 @@
+let jsonCompleto;
+let campoGraf;
+var chart;
+//CONTINUAR, pone los valores en lista
+function Json1 ()  {
+ 
+  var selectElement = document.getElementById("columnas"); 
+  //selectElement.innerHTML.textContent = "";
+   //SEGUNDO ERROR
+
+  var campo = selectElement.value;
+  //console.log(jsonCompleto);
+  campoGraf = campo;
+//guardar para dos campos seleccionados como un arreglo
 
 
-   const Json1 =  async() => {
-    const request = await fetch(`http://localhost:19990/db`, {
-        method: "get",
-    });
-    let {link} = await request.json(); 
-    fetch(`${link}`)
-        .then(response => response.json())
-        .then(data => printCharts(data));
-   };
+
+  //var f = document.querySelector('.out').textContent = out;
+  sacarVal(jsonCompleto,campo);
+  //console.log(jsonCompleto);
+  //console.log(out);
 
 
-//Funcion que carga el select de repositorios
-const Json =  async() => {
-    const request = await fetch(`http://localhost:19990/consultarRepos`, {
-        method: "get",
-    });
-    var nuevoArray = new Array(1);
-    nuevoArray = await request.json(); 
-    //console.log(nuevoArray)
 
-    const lista = document.getElementById('repos');
-    lista.innerHTML = '';
-
-    for (x of nuevoArray) {
-        const lista1 = document.createElement('option');
-        lista1.textContent = x.nombre_repositorio;
-        lista1.value = x.id;
-        lista.add(lista1);
-    } 
+  //printCharts(sacarVal(jsonCompleto,out));
 };
 
 
-//Funcion que obtiene las columnas disponibles
-const Json2 =  async() => {
+//SACA LOS CAMPOS DEL JSON
+function sacarColum(repositorio){
 
-    const id = document.getElementById('repos');
+  var i = 0;
+  //var cols = new Array(1);
+  const listaColum = document.getElementById("columnas");
+  listaColum.innerHTML = "";
+
+  for (let item in repositorio[0]) {
+    if (item.startsWith("nombre_") || item.startsWith("modalidad_") || item.startsWith("formato_")) {
+      i++;
+      const lista1 = document.createElement("option");
+      lista1.textContent = item;
+      lista1.value = item;
+      listaColum.add(lista1);
+      console.log("ITEM = " + item + i);
+    }
+  }
+};
+
+//SACA LOS VALORES DEL JSON FILTRADOS POR CAMPO
+function sacarVal(repositorio,campo){
+console.log(campo + "Este es el campo pa"); //ERROR Primero
+var lista1 = null;
+ var cols2 = new Array(1);
+ const listaVal = document.getElementById("valores");
+ listaVal.innerHTML = "";
+
+ for (var i=0; i < repositorio.length; i++) {
    
-    const request = await fetch(`http://localhost:19990/consultarColumnas/${id.value}`,{
-        method: "get",
-    });
+    cols2.push(repositorio[i][`${campo}`]);
+    //console.log(repositorio[i].campo.textContent);
+ }
+//guardar en un vector los valores e ir filtrando para que no este repetido
 
-    var cols = new Array(1);
-    cols = await request.json(); 
+ let uniqueChars = [...new Set(cols2)]; //quita los repetidos
+ //console.log(uniqueChars[1]);
+ //console.log(uniqueChars);
 
-    //console.log(cols)
+ for(var i=0;i < uniqueChars.length; i++){
+  //lista1 = document.createElement("option");
+  if(uniqueChars[i] != undefined  ){
+    lista1 = document.createElement("option");
+    lista1.textContent =uniqueChars[i];
+    lista1.value = uniqueChars[i];
+    listaVal.add(lista1);
+  }//else{
+    //console.log("Ok pa");
+  //}
+ }
+ 
+ return uniqueChars;
 
-    const listaColum = document.getElementById('columnas');
-    listaColum.innerHTML = '';
-
-    for (x of cols) {
-        const lista1 = document.createElement('option');
-        lista1.textContent = x.nombre_campo;
-        lista1.value = x.id;
-        listaColum.add(lista1);
-    } 
+ 
+ //Json1(uniqueChars);
 };
 
-
-
-
-function printCharts(repositorio){
-    document.body.classList.add('running') //para que aparezca en el front los canvas y cambiar la propiedad en el css
-    document.body.classList.remove()
-   // console.log(repositorio);
-   compareRadialChart(repositorio,'chart1')
-   coursesRadialChart(repositorio,'chart2')
+//OBTIENE EL REPOSITORIO DEL LINK JSON Y SE ASIGNA A VARIABLE GLOBAL
+const sacarJson = async (id) => {
+  //mode: "no-cors"
+  let request = await fetch(`http://localhost:19990/db/${id}`, {
+    method: "get",
+    //mode: 'no-cors',
+    //credentials: 'include' 
+    //credentials: 'include'
+    
+  });
+  let { link } = await request.json();
+  request = await fetch(`${link}`);
+  let response = await request.json();
+  console.log(response)
+  let data1 = await sacarColum(response);   //ESPERAR A QUE SE RESUELVA
+  jsonCompleto=response;  
+  //.then((response) => response.json())
+    
+    //.then((data) => sacarColum(data,"nombre_ocupacion")); //"PRIMERO ERROR"
+    //console.log(data);
 }
 
-function coursesRadialChart(repositorio,id){
+//CARGA EL SELECT DE LOS REPOSITORIOS DE LA BASE DE DATOS Y LA PONE EN LA LISTA
+const Json = async () => {
+  document.body.classList.add('running1')
+  
+  const request = await fetch(`http://localhost:19990/consultarRepos`, {
+    method: "get",
+  });
+  var nuevoArray = new Array(1);
+  nuevoArray = await request.json();
+  //console.log(nuevoArray)
 
-    //const courses = repositorio.filter(eachCourse => eachCourse.total_aprendices_activos > 50 );
-    
-    const data = {
-              //parametros de data
-                labels: ['Presencial','Virtual'],
-                datasets:[
-                {
-                    //for arreglo de valores
-                    //label: ['virtual', 'presencial'],
-                    data: [           
-                        repositorio.filter(eachData => eachData.modalidad_formacion === 'PRESENCIAL').length, 
-                        repositorio.filter(eachData => eachData.modalidad_formacion === 'VIRTUAL').length,              
-                    ],
-                    //data:courses.map(eachCourse => eachCourse.total_aprendices_activos),
-                    borderColor:styles.color.solids.map(eachColor => eachColor),
-                    backgroundColor:styles.color.alphas.map(eachColor => eachColor),
-                    borderWidth: 1
-                }]
-            }
+  
 
-    const options = {   //estilos y opciones del grafico
-                scale:{
-                    gridLines:{ //lineas 
-                        color: '#444'
-                    },
-                    pointLabels:{
-                        fontColor: '#fff'
-                    },
-                    ticks:{ //quitar etiquetas 
-                        display: false
-                    }
-                },
-                legend:{
-                    position:'right',
-                    labels:{
-                        fontColor:'#fff'
-                    }
-                }
-            }
-        
-        new Chart(id,{type: 'polarArea',data, options});
-    };
-    
-function compareRadialChart(repositorio, id){  //función para grafico radial
-    const data = {  //parametros de data
-        labels: ['La estrella', 'Caldas', 'Itagui','Medellin', 'Bello'],
-        datasets:[{
-            //for arreglo de valores
-            data: [
-                repositorio.filter(eachData => eachData.nombre_municipio_curso === 'LA ESTRELLA').length,
-                repositorio.filter(eachData => eachData.nombre_municipio_curso === 'CALDAS').length,
-                repositorio.filter(eachData => eachData.nombre_municipio_curso === 'ITAGUI').length,
-                repositorio.filter(eachData => eachData.nombre_municipio_curso === 'MEDELLIN').length,
-                repositorio.filter(eachData => eachData.nombre_municipio_curso === 'BELLO').length
-            ],
-            borderWidth:1,
-            borderColor:styles.color.solids.map(eachColor => eachColor),
-            backgroundColor:styles.color.alphas.map(eachColor => eachColor),
-        }]
+  const lista = document.getElementById("repos");
+  lista.innerHTML = "";
+  //var selectElement = document.getElementById("repos"); 
+  
+
+  for (x of nuevoArray) {
+    const lista1 = document.createElement("option");
+    lista1.textContent = x.nombre_repositorio;
+    lista1.value = x.id;
+    lista.add(lista1);
+    //console.log(lista1.value);
+  }
+
+};
+
+//Funcion que obtiene las columnas disponibles CONTINUAR
+const Json2 = () => {
+  //Validar los campos de json obteniendo el json
+  document.body.classList.add("running1");
+
+  var selectElement = document.getElementById("repos"); 
+
+  var repo = selectElement.value;
+
+  console.log(repo);
+
+  sacarJson(repo);
+  //sacarColum(jsonCompleto);
+/*
+const id = document.getElementById("repos");
+  const request = await fetch(
+    `http://localhost:19990/consultarColumnas/${id.value}`,
+    {
+      method: "get",
     }
+    
+  );
+  var cols = new Array(1);
+  cols = await request.json();
 
-    const options = {   //estilos y opciones del grafico
-        scale:{
-            gridLines:{ //lineas 
-                color: '#444'
-            },
-            ticks:{ //quitar etiquetas 
-                display: false
-            }
-        },
-        legend:{
-            position:'right',
-            labels:{
-                fontColor:'#fff'
-            }
+  //console.log(cols)
+
+  const listaColum = document.getElementById("columnas");
+  listaColum.innerHTML = "";
+
+  for (x of cols) {
+    const lista1 = document.createElement("option");
+    lista1.textContent = x.nombre_campo;
+    lista1.value = x.id;
+    listaColum.add(lista1);
+  }
+  */
+};
+
+
+//const Json3 = () => {
+  //Validar los valores del campo obteniendo el json
+  
+  //var selectElement = document.getElementById("valores"); 
+  //var valor = selectElement.value;
+
+
+//Funcion que obtiene los valores posibles a graficar CONTINUAR 2
+document.getElementById('submit').onclick = function() {
+    document.body.classList.add("running1");
+    var selected = [];
+    for (var option of document.getElementById('valores').options)
+    {
+        if (option.selected) {
+            selected.push(option.value);
         }
     }
+    //console.log(selected);
+    //if repo = primerRepo use este printchart si no use el otro, si escogio dos campos y el repo es Tal, grafique esto
 
-    new Chart(id,{type: 'polarArea',data, options})
+    //darle la opcion al usuario de escoger la grafica que quiere, hay que guardar la opcion del usuario y pasarlo como parametro en 
+    //printcharts, ya en el metodo de graficación se hara un if por cada grafico disponible por repositorio
+
+    //ocupacion y formacion no van juntos por ejemplo, enn el if se va una condicion de error que imprime que no se puede graficar
+
+
+    //por campo seleccionado escoger el texto despues del _ y concantenarlo en caso de que se escogiera otro campo con un "por"
+    //ejemplo: Centro por region
+
+
+    printCharts(jsonCompleto,selected);
+}
+
+
+function printCharts(repositorio,valor) {
+
+  //AQUI DEBE IR UN IF COMO EL DEL CRITERIO PARA QUE DEPENDIENDO DEL REPOSITORIO SELECCIONE EL METODO DE GRAFICACIÓN
+
+  document.body.classList.add("running"); //para que aparezca en el front los canvas y cambiar la propiedad en el css
+  console.log(repositorio);
+  console.log(valor);
+  console.log(campoGraf);
+    //recorrer repositorio total, if (item=campo seleccionado en la lista) =
+  //compareRadialChart(repositorio, "chart1", valor);
+  coursesRadialChart(repositorio, "chart2",valor,campoGraf);
+};
+
+function coursesRadialChart(repositorio, id, valor,campo) {
+ // const context = canvas.getContext(`${id}`);
+ 
+  if(chart != undefined ||  chart != null) 
+  {chart.destroy();}
+ // context.clearRect(0, 0, canvas.width, canvas.height);
+  //const courses = repositorio.filter(eachCourse => eachCourse.total_aprendices_activos > 50 );
+  //console.log(valor.length);
+  const labels = [];
+  for(let i= 0; i<valor.length; i++){
+     labels[i] = valor[i];
+  };
+
+var datos = [];
+
+  for(let i= 0; i<valor.length; i++){
+    
+    datos[i] =repositorio.filter(
+      (eachData) => eachData[`${campo}`] === `${valor[i]}`
+      ).length
+      console.log(datos);
+    };
+  
+ console.log(datos);
+  
+
+  const data = {
+    //parametros de data
+    labels: labels,
+    
+    datasets: [
+      {
+        //for arreglo de valores
+        //label: ['virtual', 'presencial'],
+        data: datos,
+        //data:courses.map(eachCourse => eachCourse.total_aprendices_activos),
+        borderColor: styles.color.solids.map((eachColor) => eachColor),
+        backgroundColor: styles.color.alphas.map((eachColor) => eachColor),
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    //estilos y opciones del grafico
+    scale: {
+      gridLines: {
+        //lineas
+        color: "#444",
+      },
+      pointLabels: {
+        fontColor: "#fff",
+      },
+      ticks: {
+        //quitar etiquetas
+        display: false,
+      },
+    },
+    legend: {
+      position: "right",
+      labels: {
+        fontColor: "#fff",
+      },
+    },
+  };
+
+  //event.preventDefault();
+    //var parent = document.getElementById(id);
+    //var child = document.getElementById(id);          
+    //parent.remove(child);            
+    //parent.appendChild ='<canvas id="chart2"></canvas>'; 
+
+  chart = new Chart(id, { type: "polarArea", data, options });
+
+  
+  
+
+};
+
+
+
+
+function compareRadialChart(repositorio, id,valor) {
+  //función para grafico radial
+  //console.log(repositorio[0]);
+  /*
+    if(item )
+    */
+
+  const data = {
+    //parametros de data
+    labels: ["La estrella", "Caldas", "Itagui", "Medellin", "Bello"],
+    datasets: [
+      {
+        //for arreglo de valores
+        data: [
+          repositorio.filter(
+            (eachData) => eachData.nombre_municipio_curso === "LA ESTRELLA"
+          ).length,
+          repositorio.filter(
+            (eachData) => eachData.nombre_municipio_curso === "CALDAS"
+          ).length,
+          repositorio.filter(
+            (eachData) => eachData.nombre_municipio_curso === "ITAGUI"
+          ).length,
+          repositorio.filter(
+            (eachData) => eachData.nombre_municipio_curso === "MEDELLIN"
+          ).length,
+          repositorio.filter(
+            (eachData) => eachData.nombre_municipio_curso === "BELLO"
+          ).length,
+        ],
+        borderWidth: 1,
+        borderColor: styles.color.solids.map((eachColor) => eachColor),
+        backgroundColor: styles.color.alphas.map((eachColor) => eachColor),
+      },
+    ],
+  };
+
+  const options = {
+    //estilos y opciones del grafico
+    scale: {
+      gridLines: {
+        //lineas
+        color: "#444",
+      },
+      ticks: {
+        //quitar etiquetas
+        display: false,
+      },
+    },
+    legend: {
+      position: "right",
+      labels: {
+        fontColor: "#fff",
+      },
+    },
+  };
+
+  new Chart(id, { type: "polarArea", data, options });
 };
